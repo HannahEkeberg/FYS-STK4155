1db4625f9c4754881bab50add74ffbcbc585faa6
HannahEkeberg <54843692+HannahEkeberg@users.noreply.github.com>
2019-10-07 22:07:41 +0200

    Update and rename Master_Class.py to Class.py
    Clean up

--- Project1/Class.py
+++ Project1/Class.py
@@ -16,6 +16,22 @@ from sklearn.pipeline import make_pipeline
 from sklearn.utils import resample
 
 
+"""
+Program containts 3 different classes, regression for OLS, ridge and lasso, error analysis and resampling for K-fold cross validation and 
+bootstrap. 
+
+The regression class takes in arguments; model, designmatrix X and data z when calling on the class. In the function predict,
+the predicted data is predicted with a design matrix of choice. The program is a little tricky, because it is hard to vary values of lambda for
+ridge and lasso.
+
+The error class simply takes model and data as argument and predicts the error thereby. 
+
+The resampling class takes model, design matrix X, variable x and variable y, z (f(x,y)) as arguments. Thereby, one can choose method
+K fold cross validation or bootstrap, which returns the error analysis on the train and test data. 
+
+"""
+
+
 
 class Regression:
 
@@ -25,26 +41,15 @@ class Regression:
         self.X = X
         self.z = z
 
-    #def X(self, x,y, degree=6):
-    #    X_ = np.c_[x,y]
-    #    poly = PolynomialFeatures(degree=degree)
-    #    self.X = poly.fit_transform(X_)
-
-    #def OLS(self):
-    #    self.beta_OLS = np.linalg.pinv(self.X.T.dot(self.X)) @ self.X.T.dot(self.z)
-
-
 
     def OLS(self, get_beta=False):
         self.beta_OLS = np.linalg.pinv(self.X.T.dot(self.X)) @ self.X.T.dot(self.z)
-        #print("betashape", np.shape(self.beta_OLS))
-        #print(self.beta_OLS)
         if get_beta==True:
             return self.beta_OLS
 
 
 
-    def Ridge(self, lamb=0.01, get_beta=False):
+    def Ridge(self, lamb=1e-9, get_beta=False):
         n,p=np.shape(self.X)
         I_lambda = np.identity(p, dtype=None)*lamb
         self.beta_ridge = np.linalg.inv(self.X.T.dot(self.X) + I_lambda) @ (self.X.T.dot(self.z))
@@ -52,18 +57,16 @@ class Regression:
             return self.beta_ridge
 
 
-    def Lasso(self, lamb=0.01, get_beta=False):
+    def Lasso(self, lamb=1e-5, get_beta=False):
         self.beta_Lasso = Lasso(alpha=lamb).fit(self.X, self.z).coef_
         if get_beta==True:
             return self.beta_Lasso
-
-
 
     def predict(self, X):
         if self.model == 'OLS':
             self.OLS()
             return X @ self.beta_OLS
-        elif self.model == 'ridge':
+        elif self.model == 'Ridge':
             self.Ridge()
             return X @ self.beta_ridge
         elif self.model == 'Lasso':
@@ -95,7 +98,7 @@ class Error:
 
 class Resample:
 
-    def __init__(self, model, X, x, y,z):
+    def __init__(self, model, X, x, y, z):
         self.model = model
         self.X = X#.astype('float64')
         self.x = x
@@ -112,13 +115,13 @@ class Resample:
         error = np.zeros((k)); error_test = np.zeros((k))
         bias = np.zeros((k)); bias_test = np.zeros((k))
         variance = np.zeros((k))
-        R2 = np.zeros((k))
+        R2_train = np.zeros((k))
+        R2_test = np.zeros((k))
         MSE_test = np.zeros((k))
         MSE_train = np.zeros((k))
 
         i = 0
         for train_index, test_index in kf.split(self.z): #splts len(z) times?
-            print(kf.split(self.z))
             x_train, x_test = self.x[train_index], self.x[test_index]
             y_train, y_test = self.y[train_index], self.y[test_index]
             z_train, z_test = self.z[train_index], self.z[test_index]
@@ -127,44 +130,33 @@ class Resample:
             if self.model == 'OLS':
                 zPred_Te = Regression('OLS', X_train, z_train).predict(X_test)
                 zPred_Tr = Regression('OLS', X_train, z_train).predict(X_train)
-                #return zPred_Te
             elif self.model == 'Ridge':
                 zPred_Te = Regression('Ridge', X_train, z_train).predict(X_test)
+                zPred_Tr = Regression('Ridge', X_train, z_train).predict(X_train)
             elif self.model == 'Lasso':
                 zPred_Te = Regression('Lasso', X_train, z_train).predict(X_test)
+                zPred_Tr = Regression('Lasso', X_train, z_train).predict(X_train)
             else:
                 return "Choose valid method"
 
-            error[i] = np.mean((z_test-zPred_Te)**2)
-            bias[i] = Error(z_test, zPred_Te).bias()
-            variance[i] = Error(z_test, zPred_Te).Var()
+
             R2_test[i] = Error(z_test, zPred_Te).R2()
             R2_train[i] = Error(z_train, zPred_Tr).R2()
             MSE_test[i] = Error(z_test, zPred_Te).MSE()
             MSE_train[i] = Error(z_train, zPred_Tr).MSE()
             i +=1
 
-
-        return np.mean(error), np.mean(bias), np.mean(variance), np.mean(R2_test), np.mean(R2_train), np.mean(MSE_test), np.mean(MSE_train)
+        else:
+            return np.mean(R2_test), np.mean(R2_train), np.mean(MSE_test), np.mean(MSE_train)
+  
 
     def Bootstrap(self, n_bootstraps):
-        #error= np.zeros((k)); error_test = np.zeros((k))
-        #bias = np.zeros((k)); bias_test = np.zeros((k))
-        #variance = np.zeros((k))
-        #R2 = np.zeros((k))
-        #MSE = np.zeros((k))
-
         z_train, z_test, X_train, X_test = train_test_split(self.z, self.X, test_size=0.2)
-        #print("ztrain shape:", np.shape(z_train))
-        #print("ztest shape:", np.shape(z_test))
         sampleSize = X_train.shape[0]
         z_pred = np.empty((z_test.shape[0], n_bootstraps))
-        #print("z pred shape: ", np.shape(z_pred))
         z_train_pred = np.empty((z_train.shape[0], n_bootstraps))
         z_train_boot = np.empty((z_train.shape[0], n_bootstraps))
-        #z = self.z.reshape(-1,1)
 
-        #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
         for i in range(n_bootstraps):
             X_,z_ = resample(X_train, z_train)
             if self.model == 'OLS':
@@ -176,69 +168,16 @@ class Resample:
             else:
                 print("Choose valid method")
 
-            #z_train_boot[:,i] = model.predict(X_test).ravel()  #model.predict(X_test)
-            #print("Shape z pred in bootstrap" ,np.shape(z_pred), i)
-            #z_train_boot = z_.ravel()        #model.predict(X_test)
 
-            z_pred = model.predict(X_test)#.ravel()
-            #print(z_pred)
-            #print(np.shape(z_pred))
-        #error = Error(z_test, z_pred).error()
-        MSE = Error(z_test, z_pred).MSE()
-        R2 = Error(z_test, z_pred).R2()
-        bias = Error(z_test, z_pred).bias()
-        Var = Error(z_test, z_pred).Var()
-        return MSE, R2, bias, Var#np.mean(MSE), np.mean(R2), np.mean(bias), np.mean(Var)
-            #z_pred_train = model.predict(X_train).ravel()
+            z_pred[:,i] = model.predict(X_test)#.ravel()
+            z_train_pred[:,i] = model.predict(X_train)
+            z_train_boot[:,i] = z_
 
-            #indices = np.random.randint(0, sampleSize, sampleSize)
-            #X_, z_ = X_train[indices], z_train[indices]
-            #z_train_boot[:,i] = z_
+        MSE_test = np.mean( np.mean((z_test[:, None] - z_pred)**2, axis=1, keepdims=True) )
+        #MSE_train = np.mean( np.mean((z_train_pred[:, None] - z_train_boot)**2, axis=1, keepdims=True))
+        MSE_train = np.mean( np.mean((z_train[:, None] - z_train_pred)**2, axis=1, keepdims=True))
+        bias = np.mean( (z_test[:,None] - np.mean(z_pred, axis=1, keepdims=True))**2 )
+        Var = np.mean( np.var(z_pred, axis=1, keepdims=True) )
 
-            #x_, y_, z_ = resample(x_train, y_train, z_train)
-            #X_new = np.c_[x_test, y_test]
-            #poly = PolynomialFeatures(degree=6)
-            #X_test = poly.fit_transform(X_new)
-
-            #if self.model == 'OLS':
-            #model_ = Regression('OLS', X_, z_).OLS().predict(X_test)
-                #z_pred = model_.predict(X_test)
-        #print(z_pred)
-        #z_test = z_test.reshape((len(z_test), 1))
-        #print(np.shape(z_test))
-        #print(z_test.ravel())
-        #error = np.mean( np.mean((z_pred - z_test)**2, axis=1, keepdims=True))
-        #print(np.shape(error))
-        #error = np.mean( np.mean((z_pred - z_test)**2, axis=1, keepdims=True))
-
-
-        #return error
-
-
-
-
-        #for degree in range(max_degree):
-            #poly = PolynomialFeatures(degree=degree)
-            #X_new = poly.fit_transform(self.X)
-
-            #OLS = Regression('OLS', self.X, self.z)
-            #m = make_pipeline(PolynomialFeatures(degree=degree), OLS)
-
-
-            #model = make_pipeline(PolynomialFeatures(degree=degree), OLS)
-            #y_pred = np.empty((y_test.shape[0], n_bootstraps))
-            #for i in range(n_bootstraps):
-        #        x_, y_ = resample(x_train, y_train)
-        #        y_pred[:,i] = model.fit(x_, y_).predict(x_test).ravel()
-
-        #    polydegree[degree]=degree
-        #    error[degree] = np.mean( np.mean((y_test - y_pred)**2, axis=1, keepdims=True) )
-        #    bias[degree] = np.mean( (y_test - np.mean(y_pred, axis=1, keepdims=True))**2 )
-        #    variance[degree] = np.mean( np.var(y_pred, axis=1, keepdims=True) )
-
-        #return polydegree, error, bias, variance
-        #return 0
-        #return OLS
-
-
+        return MSE_test, MSE_train, bias, Var
 
