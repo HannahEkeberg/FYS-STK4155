c7b1eafaee03135a2407ebd4e045ea2c8d686174
HannahEkeberg <54843692+HannahEkeberg@users.noreply.github.com>
2019-11-04 14:21:17 +0100

    Update logistic_regression.py

--- Project2/logistic_regression.py
+++ Project2/logistic_regression.py
@@ -5,16 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import random, seed
 
-from creditcard_data import *
+from sklearn.metrics import confusion_matrix   #to evaluate the accuracy of a classification.
+
+
+
+#from creditcard_data import *
 
 
 
 
-class LogisticRegression:
+class LogisticRegression_class:
 
-    def __init__(self, learning_rate, n_batches, n_epochs, lambda_=0, tol=1e-5):
+    def __init__(self, learning_rate, batch_size, n_epochs, lambda_=0, tol=1e-5):
         self.learning_rate = learning_rate
-        self.n_batches = n_batches
+        #self.n_batches = n_batches
+        self.batch_size = batch_size
         self.n_epochs = n_epochs
         self.lambda_ = lambda_
         self.tol = tol
@@ -25,70 +30,99 @@ class LogisticRegression:
         return(1/(1+np.exp(-x)))
 
     def gradient_descent(self, X, y):   #optimizing parameters beta
-        y = y.T[0]
+        y = y.T
         par = np.shape(X)[1] #number of parameters
         n = np.shape(X)[0] #number of rows
-        beta = np.random.randn(par)  #generate random values for beta
-        batch_size  = int(n/self.n_batches)
+        n_batches = n // self.batch_size #integer division
+        beta = np.random.randn(par)  #generate random initial values for beta
+        #batch_size  = int(n/self.n_batches)
         #print(batch_size)
 
 
-        C = 0 #cost function of estimated y. Set to zero before first iterarion.
+        C_data = 0 #cost function of estimated y. Set to zero before first iteration.
         error = []
 
+        n_0_list = []
+        n_1 = np.count_nonzero(y)   #total number of y=1. double check that those are really 1.
+        const = (n - n_1)/n_1
 
-        for i in range(self.n_epochs):   # complete passes through data
-            p = self.sigmoid_function(X @ beta)
-            #print(p.shape, y.shape, beta.shape)
-            #C_model = self.cost_function(p, y, beta)
-            #absolute_cost = np.abs(C - C_model)
-            #error.append(absolute_cost)
-        #print(error)
+        error = []
+        k = 0
+        #total numer of iterations:
+        for i in range(self.n_epochs):
+            y_pred = self.sigmoid_function(X @ beta)
+            #print(y_pred)
+            C = self.cost_function(y, y_pred)
+            abs_C  = np.abs(C_data - C)
+            error.append(abs_C)
 
+            for j in range(n_batches):
+                #gange gradienten med en verdi C=antall default ==0 and default==1.
+                gradient  = np.sum(X.T * self.sigmoid_function(X @ beta) - y*X)*const  #X.T @ (self.sigmoid_function(X @ beta) * const * y)  #const*0 =0, const*1 =const, gives higher val for undrerepresented 1's
+                gradient = gradient/self.batch_size
+                #print(gradient.shape)
+                beta -= self.learning_rate * gradient
+                k += 1
 
-            #index  = np.arange(n)
-            #np.random.shuffle(index)
-
-            #for k in range(self.n_batches):   #number of batches the data is divided into
-
-
-                #ouch = index[(j+1)*batch_size]
-                #print(ouch)
-                #random_index = index[j*batch_size:(j+1)*batch_size]
-                #Xi = X[random_index, :]
-                #yi = y[random_index]
-
-                #gradient  = Xi.T @ (self.sigmoid_function(Xi @ beta)* yi)
-        #print(gradient.shape)
-                #beta -= self.learning_rate * gradient
-                # += 1
-
-        self.X = X
-        self.y = y
         self.beta = beta
-        #return beta
+        y_pred = self.sigmoid_function(X @ self.beta)
+
+        self.accuracy_matrix(y, y_pred, 'Analytical method')
+        I = self.accuracy_function(y, y_pred)
+        print(error)
+        print(self.beta.shape)
+        #print("accuracy: ", I)
+        #C = self.cost_function(y, y_pred)
+        return self.beta
 
 
-    def fit_function():
-        pass
+    def fit_function(self, X_train, X_test, y_train, y_test):
+        self.gradient_descent(X_train, y_train)
+        y_pred = X_test @ self.beta
+        print(y_pred)
 
 
-    def cost_function(self, X,y,beta):
+    def predict_sklearn(self, X_train, X_test, y_train, y_test):
+        LR = LogisticRegression()
+        LR.fit(X_train,y_train)
+        y_pred = LR.predict(X_test)
+        v = np.count_nonzero(y_pred)
+        I = self.accuracy_function(y_test, y_pred)
+        C = self.cost_function(y_test, y_pred)
+        CM = self.accuracy_matrix(y_test, y_pred, 'sklearn ')
 
-        p = self.sigmoid_function(X @ beta)
-        if self.lambda_ == 0:
-            Cost = -np.sum(y.T * np.log(p) + (1-y.T)*np.log(1-p) )
+    def cost_function(self, y_data, y_pred):
+        return -np.sum(y_data.T)# * np.log(y_pred) + (1-y_data.T)*np.log(1-y_pred) )
+        #p = self.sigmoid_function(X @ beta)
+        #if self.lambda_ == 0:
+            #Cost = -np.sum(y_data.T * np.log(y_pred) + (1-y_data.T)*np.log(1-y_pred) )
         #else:
         #    Cost += np.sum(self.lambda_ * np.sum(beta**2))
-        return Cost
+        #    return Cost
 
-    def accuracy_function(self,y_data, y_fit):
-        n = np.shape(y_data)[0]
-        if np.shape(y_data)[0] == np.shape(y_fit)[0]:
-            I = np.where(y_data==y_fit)     ## =1 if y==y, 0 else
+    def accuracy_function(self,y_data, y_pred):
+        if np.shape(y_data)[0] == np.shape(y_pred)[0]:
+            I = np.mean(y_data==y_pred)     ## =1 if y==y, 0 else.... np.mean #dobbelsjekk at det funker
         else:
             raise ValuError
-        return len(np.ravel(I))/n
+        return I
+
+    def accuracy_matrix(self, y_data, y_pred, title):
+        fig, ax = plt.subplots()
+        cm = confusion_matrix(y_data, y_pred)
+        print(cm)
+        cmap = plt.cm.Blues
+        im = ax.imshow(cm, interpolation = 'nearest', cmap=cmap)
+        ax.figure.colorbar(im, ax=ax)
+        ax.set(xticks = np.arange(cm.shape[1]),
+               yticks = np.arange(cm.shape[0]),
+               title=title,
+               xlabel='predicted y value',
+               ylabel='original y value')
+        #plt.show()
+
+        return cm
+
 
 
 
@@ -96,66 +130,3 @@ if __name__ == "__main__":
     print("main")
 else:
     print("logistic_regression:")
-
-
-
-"""
-Xtrain, Xtest, Y_train_onehot, Y_test_onehot = arrange_data()
-#print(np.shape(Y_train_onehot))
-class LogisticRegression:
-
-    def __init__(self, learn_rate, iterations, size_minibatch, n ):
-        self.learn_rate = learn_rate
-        self.n = n  #numb of degrees? batches?
-        self.iter = iterations
-        self.lambda_ = 0
-
-
-    def sigmoid_function(self, x):
-        return 1./(1+np.exp(-x))
-
-    def cost_function(self, y, X, beta, lambda_):   #max likelihood:
-
-        return -np.sum( y.T * np.log(self.sigmoid(X*beta)) + (1-y.T * np.log(1-self.sigmoid(X*beta)))) + np.sum(self.lmbd*np.sum(self.beta**2))
-
-
-    def beta(self, p): #number of parameters.
-        return np.random.randn(p)
-
-    #def learning_rate(self, t,t0, t1):
-    #    return t0/(t+t1)
-
-    def gradient_descent(self,X,y):
-        seed( 30 )
-        p = np.shape(X)[1] # numb of parameters
-        rows = np.shape(X)[0]
-        y = np.random.randint(2, size = rows )  #cant understand shape of onehotencoder
-        beta = self.beta(p)  #generate initial random beta values
-        X_new = X @ beta
-        #for i in range(self.iter):
-        #    for j in range(size_minibatch):
-
-        gradient = (X_new.T @ (self.sigmoid_function(X_new) - y) - 2*self.lambda_*beta)/self.n
-
-        return gradient
-
-
-
-
-
-    def fit(self, X, y):
-        n_shape = np.shape(X)[1]
-        beta = self.get_beta(n_shape)   #parameters to fit!
-
-        for epoch in range(self.n_epochs):
-            for i in range(self.iter):
-                random_index = np.random.randint(self.iter)
-
-
-
-
-L = LogisticRegression(0.001,2,10, 10)
-#var = LogisticRegression(0.1, 4).grad_descent()
-#beta = L.get_beta()
-grad_descent = L.gradient_descent(Xtrain, Y_train_onehot)
-"""
