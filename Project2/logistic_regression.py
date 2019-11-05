0b91c11ec580335c7e94e4770cbaa9f1b3027c2d
HannahEkeberg <54843692+HannahEkeberg@users.noreply.github.com>
2019-11-05 14:55:20 +0100

    Update logistic_regression.py

--- Project2/logistic_regression.py
+++ Project2/logistic_regression.py
@@ -1,4 +1,4 @@
-
+from part1 import arrange_data
 from sklearn.linear_model import LogisticRegression
 
 import numpy as np
@@ -16,8 +16,8 @@ from sklearn.metrics import confusion_matrix   #to evaluate the accuracy of a cl
 
 class LogisticRegression_class:
 
-    def __init__(self, learning_rate, batch_size, n_epochs, lambda_=0, tol=1e-5):
-        self.learning_rate = learning_rate
+    def __init__(self, batch_size, n_epochs, lambda_=0, tol=1e-5):
+        #self.learning_rate = learning_rate
         #self.n_batches = n_batches
         self.batch_size = batch_size
         self.n_epochs = n_epochs
@@ -35,16 +35,15 @@ class LogisticRegression_class:
         n = np.shape(X)[0] #number of rows
         n_batches = n // self.batch_size #integer division
         beta = np.random.randn(par)  #generate random initial values for beta
-        #batch_size  = int(n/self.n_batches)
-        #print(batch_size)
 
 
-        C_data = 0 #cost function of estimated y. Set to zero before first iteration.
+
+
         error = []
 
         n_0_list = []
         n_1 = np.count_nonzero(y)   #total number of y=1. double check that those are really 1.
-        const = (n - n_1)/n_1
+        #const = (n - n_1)/n_1
 
         error = []
         k = 0
@@ -53,33 +52,35 @@ class LogisticRegression_class:
             y_pred = self.sigmoid_function(X @ beta)
             #print(y_pred)
             C = self.cost_function(y, y_pred)
-            abs_C  = np.abs(C_data - C)
-            error.append(abs_C)
+            #abs_C  = np.abs(C_data - C)
+            error.append(C)
 
             for j in range(n_batches):
                 #gange gradienten med en verdi C=antall default ==0 and default==1.
-                gradient  = np.sum(X.T * self.sigmoid_function(X @ beta) - y*X)*const  #X.T @ (self.sigmoid_function(X @ beta) * const * y)  #const*0 =0, const*1 =const, gives higher val for undrerepresented 1's
+                gradient  = np.sum(X.T * self.sigmoid_function(X @ beta) - y*X) #X.T @ (self.sigmoid_function(X @ beta) * const * y)  #const*0 =0, const*1 =const, gives higher val for undrerepresented 1's
                 gradient = gradient/self.batch_size
                 #print(gradient.shape)
-                beta -= self.learning_rate * gradient
+                learning_rate = self.learning_schedule(i*n_batches+j)     #adaptive learning rate. Sjekke om det enkelste funker forst.
+                beta -= learning_rate * gradient
                 k += 1
 
         self.beta = beta
         y_pred = self.sigmoid_function(X @ self.beta)
+        tol = 0.5
+        y_pred[y_pred >= tol] = 1
+        y_pred[y_pred < tol] = 0
 
-        self.accuracy_matrix(y, y_pred, 'Analytical method')
-        I = self.accuracy_function(y, y_pred)
-        print(error)
-        print(self.beta.shape)
-        #print("accuracy: ", I)
-        #C = self.cost_function(y, y_pred)
-        return self.beta
+        I = accuracy_function(y, y_pred)
 
+        return error, self.n_epochs
+
+    def learning_schedule(self,t):  #??? what is this
+        t0 = 5; t1 = 50
+        return t0/(t+t1)
 
     def fit_function(self, X_train, X_test, y_train, y_test):
         self.gradient_descent(X_train, y_train)
         y_pred = X_test @ self.beta
-        print(y_pred)
 
 
     def predict_sklearn(self, X_train, X_test, y_train, y_test):
@@ -89,18 +90,14 @@ class LogisticRegression_class:
         v = np.count_nonzero(y_pred)
         I = self.accuracy_function(y_test, y_pred)
         C = self.cost_function(y_test, y_pred)
-        CM = self.accuracy_matrix(y_test, y_pred, 'sklearn ')
+        print(I)
+        CM = self.accuracy_matrix(y_test, y_pred, 'sklearn')
 
-    def cost_function(self, y_data, y_pred):
-        return -np.sum(y_data.T)# * np.log(y_pred) + (1-y_data.T)*np.log(1-y_pred) )
-        #p = self.sigmoid_function(X @ beta)
-        #if self.lambda_ == 0:
-            #Cost = -np.sum(y_data.T * np.log(y_pred) + (1-y_data.T)*np.log(1-y_pred) )
-        #else:
-        #    Cost += np.sum(self.lambda_ * np.sum(beta**2))
-        #    return Cost
+    def cost_function(self, y_data, y_pred):  #oppdatere parametrene.
+        return np.sum( (y_data.T * y_pred ) - np.log(1+np.exp(y_pred)))
+        #return -np.sum(y_data.T)* np.log(y_pred) + (1-y_data.T)*np.log(1-y_pred))
 
-    def accuracy_function(self,y_data, y_pred):
+    def accuracy_function(self,y_data, y_pred):  #hvor bra modellen gjor det.
         if np.shape(y_data)[0] == np.shape(y_pred)[0]:
             I = np.mean(y_data==y_pred)     ## =1 if y==y, 0 else.... np.mean #dobbelsjekk at det funker
         else:
@@ -119,9 +116,11 @@ class LogisticRegression_class:
                title=title,
                xlabel='predicted y value',
                ylabel='original y value')
-        #plt.show()
+        plt.show()
 
         return cm
+
+
 
 
 
